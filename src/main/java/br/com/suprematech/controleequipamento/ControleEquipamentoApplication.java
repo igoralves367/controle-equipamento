6e4ca0957dc8b6159cb3d9dbb272a4a72a227f23
igoralves367 <igor.alves367@gmail.com>
2023-01-26 14:01:22 -0300

    Implementa ControleEquipamentoApplication.java, teste Ok

--- src/main/java/br/com/suprematech/controleequipamento/ControleEquipamentoApplication.java
+++ src/main/java/br/com/suprematech/controleequipamento/ControleEquipamentoApplication.java
@@ -2,12 +2,15 @@ package br.com.suprematech.controleequipamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ControleEquipamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleEquipamentoApplication.class, args);
 	}
-
 }
