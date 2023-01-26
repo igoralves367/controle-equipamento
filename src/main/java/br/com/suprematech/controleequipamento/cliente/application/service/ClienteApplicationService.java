940601ee4152e6a0f9f6a486465a24dcf2a40162
igoralves367 <igor.alves367@gmail.com>
2023-01-26 16:16:24 -0300

    Implementa Service e ApplicationService

--- src/main/java/br/com/suprematech/controleequipamento/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/suprematech/controleequipamento/cliente/application/service/ClienteApplicationService.java
@@ -1,7 +1,10 @@
 package br.com.suprematech.controleequipamento.cliente.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.com.suprematech.controleequipamento.cliente.application.api.ClienteListResponse;
 import br.com.suprematech.controleequipamento.cliente.application.api.ClienteRequest;
 import br.com.suprematech.controleequipamento.cliente.application.api.ClienteResponse;
 import br.com.suprematech.controleequipamento.cliente.application.reposiory.ClienteRepository;
@@ -22,5 +25,12 @@ public class ClienteApplicationService implements ClienteService {
 		return ClienteResponse.builder()
 				.idCliente(cliente.getIdCliente())
 				.build(); 
+	}
+
+	@Override
+	public List<ClienteListResponse> buscarTodosClientes() {
+		log.info("[inicia] ClienteApplicationService - buscarTodosClientes");
+		log.info("[finaliza] ClienteApplicationService - buscarTodosClientes");
+		return null;
 	}
 }
--- src/main/java/br/com/suprematech/controleequipamento/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/suprematech/controleequipamento/cliente/application/service/ClienteService.java
@@ -1,8 +1,12 @@
 package br.com.suprematech.controleequipamento.cliente.application.service;
 
+import java.util.List;
+
+import br.com.suprematech.controleequipamento.cliente.application.api.ClienteListResponse;
 import br.com.suprematech.controleequipamento.cliente.application.api.ClienteRequest;
 import br.com.suprematech.controleequipamento.cliente.application.api.ClienteResponse;
 
 public interface ClienteService {
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
+	List<ClienteListResponse> buscarTodosClientes();
 }
