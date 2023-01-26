f75f720e8ab8a5dce4be842d4ae03a04f5608f46
igoralves367 <igor.alves367@gmail.com>
2023-01-26 14:21:32 -0300

    Implementa Dominio de Negocio da Classe Cliente

---
+++ src/main/java/br/com/suprematech/controleequipamento/cliente/domain/Cliente.java
@@ -0,0 +1,37 @@
+package br.com.suprematech.controleequipamento.cliente.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Cliente")
+public class Cliente {
+	@Id
+	private UUID idCliente;
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String cpfCnpj;
+
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+}
