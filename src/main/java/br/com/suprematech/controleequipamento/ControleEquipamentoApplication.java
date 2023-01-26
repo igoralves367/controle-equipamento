ef8007ffaa83fdbbebe7a6e783c8002b85a06623
igoralves367 <igor.alves367@gmail.com>
2023-01-26 13:46:42 -0300

    Inicia Projeto

---
+++ src/main/java/br/com/suprematech/controleequipamento/ControleEquipamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.suprematech.controleequipamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEquipamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEquipamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/suprematech/controleequipamento/ControleEquipamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suprematech.controleequipamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEquipamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
