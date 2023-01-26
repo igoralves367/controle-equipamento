acc10020cb8a9c18a12efd79ddcc8916101f1de9
igoralves367 <igor.alves367@gmail.com>
2023-01-26 14:30:47 -0300

    Implementa Controller ate Service

---
+++ src/main/java/br/com/suprematech/controleequipamento/cliente/application/service/ClienteService.java
@@ -0,0 +1,8 @@
+package br.com.suprematech.controleequipamento.cliente.application.service;
+
+import br.com.suprematech.controleequipamento.cliente.application.api.ClienteRequest;
+import br.com.suprematech.controleequipamento.cliente.application.api.ClienteResponse;
+
+public interface ClienteService {
+	ClienteResponse criaCliente(ClienteRequest clienteRequest);
+}
