4c4f214bb58aa7797b1c903e5994de50d20e2824
igoralves367 <igor.alves367@gmail.com>
2023-01-26 16:26:50 -0300

    Implementa ApplicationSerivce ate MongoRepository e Cria Lista em ListResponse

--- src/main/java/br/com/suprematech/controleequipamento/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/suprematech/controleequipamento/cliente/application/api/ClienteListResponse.java
@@ -1,7 +1,9 @@
 package br.com.suprematech.controleequipamento.cliente.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
+import br.com.suprematech.controleequipamento.cliente.domain.Cliente;
 import lombok.Value;
 @Value
 public class ClienteListResponse {
@@ -10,4 +12,9 @@ public class ClienteListResponse {
 	private String email;
 	private String celular;
 	private String cpfCnpj;
+	
+	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/main/java/br/com/suprematech/controleequipamento/cliente/application/reposiory/ClienteRepository.java
+++ src/main/java/br/com/suprematech/controleequipamento/cliente/application/reposiory/ClienteRepository.java
@@ -1,7 +1,10 @@
 package br.com.suprematech.controleequipamento.cliente.application.reposiory;
 
+import java.util.List;
+
 import br.com.suprematech.controleequipamento.cliente.domain.Cliente;
 
 public interface ClienteRepository {
 	Cliente salva(Cliente cliente);
+	List<Cliente> buscaTodosClientes();
 }
