90e4c35d35ba4323764cee5eca5e2c2d8295eca0
igoralves367 <igor.alves367@gmail.com>
2023-01-28 19:27:12 -0300

    Altera Controller e Service[

--- src/main/java/br/com/suprematech/controleequipamento/equipamento/application/api/EquipamentoController.java
+++ src/main/java/br/com/suprematech/controleequipamento/equipamento/application/api/EquipamentoController.java
@@ -1,7 +1,5 @@
 package br.com.suprematech.controleequipamento.equipamento.application.api;
 
-import java.util.UUID;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.suprematech.controleequipamento.equipamento.application.service.EquipamentoService;
@@ -14,10 +12,9 @@ public class EquipamentoController implements EquipamentoAPI {
 	private final EquipamentoService equipamentoService;
 
 	@Override
-	public EquipamentoResponse postEquipamento(UUID idCliente, EquipamentoRequest equipamentoRequest) {
+	public EquipamentoResponse postEquipamento(EquipamentoRequest equipamentoRequest) {
 		log.info("[inicia] EquipamentoController - postEquipamento");
-		log.info("[idCliente] {}", idCliente);
-		EquipamentoResponse equipamento = equipamentoService.criaEquipamento(idCliente, equipamentoRequest);
+		EquipamentoResponse equipamento = equipamentoService.criaEquipamento(equipamentoRequest);
 		log.info("[finaliza] EquipamentoController - postEquipamento");
 		return equipamento;
 	}
