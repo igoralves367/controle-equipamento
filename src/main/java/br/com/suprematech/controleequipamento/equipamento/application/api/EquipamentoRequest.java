a1b18ecfeb044e77f4885df2acf5aaa89f489aa5
igoralves367 <igor.alves367@gmail.com>
2023-01-28 19:00:53 -0300

    Implementa Request e Response

--- src/main/java/br/com/suprematech/controleequipamento/equipamento/application/api/EquipamentoRequest.java
+++ src/main/java/br/com/suprematech/controleequipamento/equipamento/application/api/EquipamentoRequest.java
@@ -1,5 +1,17 @@
 package br.com.suprematech.controleequipamento.equipamento.application.api;
 
-public class EquipamentoRequest {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
+import lombok.Value;
+@Value
+public class EquipamentoRequest {
+	@NotNull
+	private String tipoDeImpressora;
+	@NotNull
+	private String marca;
+	@NotBlank
+	private String modelo;
+	@NotNull
+	private String numeroDeSerie;
 }
--- src/main/java/br/com/suprematech/controleequipamento/equipamento/application/api/EquipamentoResponse.java
+++ src/main/java/br/com/suprematech/controleequipamento/equipamento/application/api/EquipamentoResponse.java
@@ -1,5 +1,11 @@
 package br.com.suprematech.controleequipamento.equipamento.application.api;
 
-public class EquipamentoResponse {
+import java.util.UUID;
 
+import lombok.Builder;
+import lombok.Value;
+@Value
+@Builder
+public class EquipamentoResponse {
+	private UUID idEquipamento;
 }
