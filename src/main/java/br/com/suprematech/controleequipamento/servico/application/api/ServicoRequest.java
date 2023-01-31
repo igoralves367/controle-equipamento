102218b2a8768c87ac0463a657a3602d5fdbf421
igoralves367 <igor.alves367@gmail.com>
2023-01-31 18:21:01 -0300

    Altera Request

--- src/main/java/br/com/suprematech/controleequipamento/servico/application/api/ServicoRequest.java
+++ src/main/java/br/com/suprematech/controleequipamento/servico/application/api/ServicoRequest.java
@@ -2,11 +2,16 @@ package br.com.suprematech.controleequipamento.servico.application.api;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Value;
 @Value
 public class ServicoRequest {
 	private UUID idEquipamento;
+	@NotBlank
 	private String acessoriosDeixados;
+	@NotBlank
 	private String tipoDeServico;
+	@NotBlank
 	private String observacoes;	
 }
