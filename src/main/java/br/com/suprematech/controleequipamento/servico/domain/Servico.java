cb7f01fb9e182f79dd3acd6f9fed22306b74363c
igoralves367 <igor.alves367@gmail.com>
2023-01-31 18:18:55 -0300

    Ajusta Servico domain, acrescentando 02 requisitos nao funcionais

--- src/main/java/br/com/suprematech/controleequipamento/servico/domain/Servico.java
+++ src/main/java/br/com/suprematech/controleequipamento/servico/domain/Servico.java
@@ -1,6 +1,9 @@
 package br.com.suprematech.controleequipamento.servico.domain;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -22,7 +25,13 @@ public class Servico {
 	@Id
 	private UUID idServico;
 	private UUID idEquipamento;
+	@NotBlank
 	private String acessoriosDeixados;
+	@NotBlank
 	private String tipoDeServico;
+	@NotBlank
 	private String observacoes;	
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
 }
